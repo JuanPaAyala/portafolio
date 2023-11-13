50dbc7b4d36b46d8d8b7d51b3617c1518116d3ad
JUANAYALA <juanpabloayalaarboleda@gmail.com>
2023-11-13 00:14:24 -0500

    Ajuste en el formulario del contacto

--- Aplicaciones/contacto/views.py
+++ Aplicaciones/contacto/views.py
@@ -12,10 +12,8 @@ def contacto(request):
         tphone = request.POST["phone"]
         tmessage = request.POST["message"]
         send_mail(
-            'Nombre',
-            tname,
-            'Mensaje',
-            tmessage,
+            # 'Nombre', tname,
+            'Mensaje', tmessage,
             'settings.EMAIL_HOST_USER',
             [temail],
             fail_silently=False)
@@ -25,7 +23,57 @@ def contacto(request):
     #mis_proyectos = Proyecto.objects.all()
     return render(request,"pages/contact.html",)
 
-# def send_email(request):
-#     print("Sending email")
-#     send_email_test()
-#     return render(request,"pages/gracias.html",)
+
+# def contacto(request):
+#     if request.method == "POST":
+#         tname = request.POST["name"]
+#         temail = request.POST["email"]
+#         tphone = request.POST["phone"]
+#         tmessage = request.POST["message"]
+#         ctx = {
+#             'Nombre', tname,
+#             'Mensaje', tmessage,
+#             #'email', temail
+#         }
+#         message = render_to_string('mail.html', ctx)
+#         send_mail('Gracias Por Contactarnos',
+#             message,
+#             settings.EMAIL_HOST_USER,
+#             ['projecthouse2120@gmail.com'], 
+#             fail_silently=False, html_message=message)
+#         obj_contact = Contacto(name=tname,email=temail,phone=tphone,message=tmessage)
+#         obj_contact.save()    
+#         #return HttpResponse("EL registro fue ingresado")
+#         return render(request,"pages/gracias.html",)
+#     #mis_proyectos = Proyecto.objects.all()
+#     return render(request,"pages/contact.html",)
+
+# from django.shortcuts import render, HttpResponse
+# from django.core.mail import send_mail
+# from django.conf import settings
+
+# from .models import Contacto
+
+# # Create your views here.
+# def contacto(request):
+#     if request.method == "POST":
+#         tname = request.POST["name"]
+#         temail = request.POST["email"]
+#         tphone = request.POST["phone"]
+#         tmessage = request.POST["message"]
+#         obj_contact = Contacto(name=tname,email=temail,phone=tphone,message=tmessage)
+#         obj_contact.save()
+#         #return HttpResponse("EL registro fue ingresado")
+#         email(obj_contact)
+#         return render(request,"pages/gracias.html",)
+#     #mis_proyectos = Proyecto.objects.all()
+#     return render(request,"pages/contact.html",)
+
+# def email(email):
+#     subject = 'Thank you for contact me'
+#     message = ' it  means a world to us '
+#     email_from = settings.EMAIL_HOST_USER
+#     recipient_list = ['projecthouse2120@gmail.com',]
+#     send_mail( subject, message, email_from, recipient_list )
+#     return True
+    
